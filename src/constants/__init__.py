2f6b1cf738cf2418445ee1a021fc3683e9caf22b
Swapnil Kangralkar <swapnilforcat@gmail.com>
2026-02-12 19:07:44 -0500

    transformation and training done

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -59,7 +59,7 @@ MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
-MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+# Hyperparameter tuning - you can get the best parameters using grid search CV in your experimental notebook
 MODEL_TRAINER_N_ESTIMATORS=200
 MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
 MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -45,7 +45,6 @@ class ModelTrainerConfig:
     model_trainer_dir: str = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR_NAME)
     trained_model_file_path: str = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILE_NAME)
     expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
-    model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
     _n_estimators = MODEL_TRAINER_N_ESTIMATORS
     _min_samples_split = MODEL_TRAINER_MIN_SAMPLES_SPLIT
     _min_samples_leaf = MODEL_TRAINER_MIN_SAMPLES_LEAF
